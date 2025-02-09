91fd7ff03c6713f7d7d7d1eecbe59e0009dac60c
basivireddy <basivi.info016@gmail.com>
2025-02-09 17:06:41 +0530

    Initial commit

---
+++ TMDB/TMDB/TMDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDBApp.swift
+//  TMDB
+//
+//  Created by Basivi Reddy on 09/02/25.
+//
+
+import SwiftUI
+
+@main
+struct TMDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
